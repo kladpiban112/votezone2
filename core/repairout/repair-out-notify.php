b7fb38667d4950b97f32236c5ed3944b0266ef45
Kwanchaiz <Kwanchaiz2543@gmail.com>
2022-03-21 14:33:44 +0700

    Kwanchai

--- core/repairout/repair-out-notify.php
+++ core/repairout/repair-out-notify.php
@@ -16,7 +16,7 @@ $func_orgprofile = getOrgProfile($org_id);
 $line_token_key = "3HC0yOTARTdWkA7bYyHCf7aat8ys6wU2uIipjKnoXE3";
 
  // line notify
-		$txt_outjob = "แจ้งซ่อมภายนอกกรุณาตรวจสอบที่ ".ADMIN_URL;
+		$txt_outjob = "มีงานแจ้งซ่อมภายนอกกรุณาตรวจสอบ ";
 		line_text($txt_outjob,$line_token_key);
 
 ?>
